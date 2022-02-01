cb6bc7105bfa9b8d5b3ac492b7e01ae5bc18d792
Jacek Kwiecień <jacek.kwiecien@schibsted.com>
2022-02-01 21:51:26 +0100

    set up signing config

--- app/src/main/java/net/techbrewery/tvphotoframe/WelcomeActivity.kt
+++ app/src/main/java/net/techbrewery/tvphotoframe/WelcomeActivity.kt
@@ -1,28 +1,32 @@
 package net.techbrewery.tvphotoframe
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import net.techbrewery.tvphotoframe.ui.theme.AppTheme
+import net.techbrewery.tvphotoframe.ui.theme.Spacing
+import net.techbrewery.tvphotoframe.ui.theme.Typography
 
 class WelcomeActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             AppTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    SignInContent()
                 }
             }
         }
@@ -30,14 +34,55 @@ class WelcomeActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    AppTheme {
-        Greeting("Android")
+private fun SignInContent() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+            .padding(Spacing.Large),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Title()
+        SignInDisclaimer()
     }
 }
+
+@Composable
+fun Title() {
+    Text(
+        text = stringResource(R.string.title_activity_welcome),
+        style = Typography.titleLarge
+    )
+}
+
+@Composable
+fun SignInDisclaimer() {
+    Text(
+        text = "In order to use Google Photos with TV Photo frame, you need to sign in into your Google account.", //FIXME
+        style = Typography.labelLarge,
+        modifier = Modifier.padding(Spacing.Small)
+    )
+}
+
+@Preview(
+    showBackground = true,
+    widthDp = 1920,
+    heightDp = 1080,
+    uiMode = UI_MODE_NIGHT_YES
+)
+@Composable
+fun SignInContentDarkPreview() {
+    AppTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            SignInContent()
+        }
+    }
+}
+
+@Preview()
+@Composable
+fun DefaultPreview() = SignInContentDarkPreview()
---
+++ app/src/main/java/net/techbrewery/tvphotoframe/ui/theme/Spacing.kt
@@ -0,0 +1,11 @@
+package net.techbrewery.tvphotoframe.ui.theme
+
+import androidx.compose.ui.unit.dp
+
+object Spacing {
+    val Small = (8 * 3).dp
+    val Medium = (8 * 4).dp
+    val Large = (8 * 5).dp
+    val XL = (8 * 6).dp
+    val XXL = (8 * 7).dp
+}
